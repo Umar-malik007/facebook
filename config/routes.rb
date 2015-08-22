faf74fdc0eb46a3dd92f48eea99cee28991aaf38
Umar Malik <umar.malik.se007@gmail.com>
2015-08-22 08:03:41 -0700

    Added gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'register', to: 'devise/registrations#new', as: :register
     get 'login', to: 'devise/sessions#new', as: :login
-    get 'logout', to: 'devise/sessions#new', as: :logout
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
   end
 
   resources :statuses
