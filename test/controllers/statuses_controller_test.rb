da3b8a964ed2f94d5cae009f5cc55972e06ea6f8
umar Malik <umar.malik.se007@gmail.com>
2015-08-25 11:18:12 +0500

    fix the register link by adding a new controller name registration

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -38,6 +38,16 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create status for the current when logged in" do
+    sign_in users(:Umar)
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:Umar) }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:Umar).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
